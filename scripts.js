bddf4079f6ebaeb3cbc20c3b562653a87b874918
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-24 19:52:53 -0600

    fix some bugs

--- scripts.js
+++ scripts.js
@@ -1,69 +1,90 @@
 document.addEventListener("DOMContentLoaded", () => {
     const display = document.querySelector('.display');
     const buttons = document.querySelectorAll("button")
-    let currentInput = '';
+    let currentInput = '0';
     let previousInput = '';
     let operation = null;
     let resetDisplay = false;
 
     function updateDisplay() {
         const paragraph = display.querySelector("p")
-        paragraph.textContent = currentInput;
+        let displayValue = currentInput;
+
+        if (displayValue.length > 16) {
+            const num = parseFloat(displayValue);
+
+            if (num > 1e16 || num < -1e16) {
+                displayValue = num.toExponential(8);
+            }
+            else {
+                displayValue = num.toPrecision(12);
+            }
+        }
+        paragraph.textContent = displayValue;
     }
 
+    document.addEventListener("keydown", (e) => {
+        let value = e.key
+        calculator(value)
+    })
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
             let value = button.value
-            if (!isNaN(value) || value === ".") {
-                if (currentInput === "0" || resetDisplay) {
-                    currentInput = value
-                    resetDisplay = false
-                } else {
-                    if (currentInput.split("").includes(".") && value === ".") {
-                        return
-                    } else if (currentInput.length < 16) {
-                        currentInput += value;
-                    }
-
-                }
-                updateDisplay();
-
-            } else if (["+", "-", "/", "*"].includes(value)) {
-                if (previousInput === "" && currentInput === "") return
-
-                if (previousInput !== "" && currentInput !== "" && operation !== null && !resetDisplay) {
-                    operate()
-                }
-                previousInput = currentInput
-                operation = value
-                resetDisplay = true
-
-
-            } else if (value === "=") {
-                if (operation !== null && !resetDisplay) {
-                    operate()
-                    operation = null;
-                    resetDisplay = true;
-                }
-            } else if (value == "ac") {
-                currentInput = '';
-                previousInput = '';
-                operation = null;
-                updateDisplay()
-
-            } else if (value === "+/-") {
-                currentInput = (parseFloat(currentInput) * -1).toString()
-                updateDisplay()
-
-            } else if (value === "%") {
-                currentInput = (parseFloat(currentInput) / 100).toString()
-                updateDisplay()
-            } else if (value === "del") {
-                currentInput = currentInput.slice(0, -1);
-                updateDisplay()
-            }
+            calculator(value)
         })
     })
+
+    function calculator(value) {
+        if (!isNaN(value) || value === ".") {
+            if (currentInput === "0" || resetDisplay) {
+                currentInput = value
+                resetDisplay = false
+            } else {
+                if (currentInput.split("").includes(".") && value === ".") {
+                    return
+                } else if (currentInput.length < 16) {
+                    currentInput += value;
+                }
+
+            }
+            updateDisplay();
+
+        } else if (["+", "-", "/", "*"].includes(value)) {
+            if (previousInput === "" && currentInput === "") return
+
+            if (previousInput !== "" && currentInput !== "" && operation !== null && !resetDisplay) {
+                operate()
+            }
+            previousInput = currentInput
+            operation = value
+            resetDisplay = true
+
+
+        } else if (value === "Enter") {
+            if (operation !== null && !resetDisplay) {
+                operate()
+                operation = null;
+                resetDisplay = true;
+            }
+        } else if (value == "Escape") {
+            currentInput = '0';
+            previousInput = '';
+            operation = null;
+            updateDisplay()
+
+        } else if (value === "+/-") {
+            currentInput = (parseFloat(currentInput) * -1).toString()
+            updateDisplay()
+
+        } else if (value === "%") {
+            currentInput = (parseFloat(currentInput) / 100).toString()
+            updateDisplay()
+        } else if (value === "Backspace") {
+            currentInput = currentInput.slice(0, -1);
+            updateDisplay()
+        }
+    }
+
 
     function add(a, b) {
         return a + b
@@ -104,7 +125,7 @@ document.addEventListener("DOMContentLoaded", () => {
             default:
                 return;
         }
-        if(result == "ERROR"){
+        if (result == "ERROR") {
             currentInput = result
         }
         else if (Number.isInteger(result)) {
@@ -115,11 +136,3 @@ document.addEventListener("DOMContentLoaded", () => {
         updateDisplay()
     }
 })
-
-
-
-
-
-
-
-
